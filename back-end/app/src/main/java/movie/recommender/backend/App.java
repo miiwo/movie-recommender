c7c3f7a4dfc7c10375a33b1236deb3a83ab98a5c
miiwo <chellewsa@gmail.com>
2021-07-01 19:08:31 -0400

    add base backend app w/ gradle

---
+++ back-end/app/src/main/java/movie/recommender/backend/App.java
@@ -0,0 +1,15 @@
+package movie.recommender.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
